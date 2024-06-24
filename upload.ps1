bdf6f923dbba24c66e027255646930f959ed9655
IInspectable <github@smjh.de>
2024-06-24 22:14:25 +0200

    Initial Checkin

---
+++ upload.ps1
@@ -0,0 +1,30 @@
+
+$settingsFile = Join-Path $PSScriptRoot 'settings.template.json'
+$uploadFolder = Join-Path $PSScriptRoot 'upload'
+
+if( -not (Test-Path $settingsFile -PathType Leaf)) {
+    throw "The settings file $settingsFile does not exist."
+}
+
+#-----------------------
+if( Test-Path $uploadFolder -PathType Container) {
+    Remove-Item $uploadFolder -Recurse -Force
+} 
+
+echo "erstelle $uploadFolder"
+mkdir $uploadFolder | Out-Null
+
+$settings = Get-Content $settingsFile | ConvertFrom-Json
+
+echo $settingsFile
+echo $uploadFolder
+
+echo $settings
+
+# ---------------------------
+$webcam1Src='https://www.mfv-peissenberg.de/images/s2dlogo.gif'
+Invoke-WebRequest $webcam1Src -OutFile "$uploadFolder/webcam1.gif"
+
+Get-ChildItem $uploadFolder -File | % {
+    echo $_.Name
+}
