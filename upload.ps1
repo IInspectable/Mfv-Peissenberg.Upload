876976f5fdec0f015f21330aefa40ec950877403
IInspectable <github@smjh.de>
2024-06-26 12:42:41 +0200

    Wettercam hinzugefügt

--- upload.ps1
+++ upload.ps1
@@ -29,9 +29,12 @@ $webcam1Src="http://mfvp.bplaced.net/$upcamFolder/images/upcam.jpg"
 Write-Verbose "Flugfeld Bild von Webcam anfordern: $webcam1Src"
 Invoke-WebRequest $webcam1Src -OutFile "$uploadFolder/Startbahn.jpg"
 
+Write-Verbose "Wetter Bild von Webcam anfordern: $webcam1Src"
+curl -u "$($settings.CameraUser):$($settings.CameraPassword)" http://wettercam.fritz.box/tmpfs/snap.jpg --output "$uploadFolder/WetterCam.jpg"
+
 #========= Dateien hochladen ================
 Get-ChildItem $uploadFolder -File | % {
 
     Write-Verbose "Lade Datei '$($_.FullName)' hoch"
-    curl -v -k "$($settings.Server)" --user "$($settings.User):$($settings.Password)" -T "$($_.FullName)"
+    curl -v -k "$($settings.UploadServer)" --user "$($settings.UploadUser):$($settings.UploadPassword)" -T "$($_.FullName)"
 }
